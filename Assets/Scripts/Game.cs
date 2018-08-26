b5845b36543bb750b43760f0a4260c46f738364f
hellbo! <alexey.s.litvinov@gmail.com>
2018-08-26 16:09:42 +0300

    pulled

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,225 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
+
+    public static Transform[,] grid = new Transform[gridWidth, gridHeight];
+
+
+
+    void Start () {
+
+
+        SpawnNextTetromino();
+	}
+	
+	public bool IsFullRowIt (int y)
+    {
+
+        for (int x = 0; x < gridWidth; ++x)
+        {
+
+            if (grid[x, y] == null) 
+                {
+
+                return false;
+
+                }
+
+        }
+
+        return true;
+
+    }
+
+    public void DeleteMinoat(int y)
+    {
+
+        for (int x = 0; x < gridWidth; ++x)
+        {
+
+            Destroy(grid[x, y].gameObject);
+
+            grid[x, y] = null;
+
+        }
+
+    }
+
+    public void MoveRowDown (int y)
+    {
+
+        for (int x = 0; x < gridWidth; ++x)
+        {
+
+            if (grid[x,y] != null)
+          {
+
+                grid[x, y - 1] = grid[x, y];
+
+                grid[x, y] = null;
+
+                grid[x, y - 1].position += new Vector3(0, -1, 0);
+
+          }
+
+        }
+
+    }
+
+    public void MoveAllRowsDown(int y)
+    {
+
+        for (int i = y; i < gridHeight; ++i)
+        {
+
+            MoveRowDown(i);
+
+        }
+
+    }
+
+    public void DeleteRow()
+    {
+
+        for (int y = 0; y < gridHeight; ++y)
+        {
+
+            if (IsFullRowIt(y))
+            {
+
+                DeleteMinoat(y);
+                MoveAllRowsDown(y + 1);
+                --y;
+
+            }
+
+        }
+
+
+
+    }
+
+
+
+    public void UpdateGrid (Tetromino tetromino)
+    {
+
+        for (int y = 0; y<gridHeight; ++y)
+        {
+
+            for (int x = 0; x < gridWidth; ++x)
+            {
+
+                if (grid[x, y] != null)
+                {
+
+                    if (grid[x, y].parent == tetromino.transform)
+
+                   {
+
+                        grid[x, y] = null;
+
+                    }
+
+                }
+
+            }
+
+
+        }
+
+        foreach (Transform mino in tetromino.transform)
+        {
+
+            Vector2 pos = Round(mino.position);
+
+            if (pos.y < gridHeight)
+            {
+
+                grid[(int)pos.x, (int)pos.y] = mino;
+
+            }
+
+        }
+
+    }
+
+    public Transform GetTransformAtGridPosition (Vector2 pos)
+    {
+
+        if (pos.y > gridHeight - 1)
+        {
+
+            return null;
+        }
+        else
+        {
+            return grid[(int)pos.x, (int)pos.y];
+         }   
+
+
+    }
+
+       
+    public void SpawnNextTetromino ()
+    {
+        GameObject NextTetromino = (GameObject)Instantiate(Resources.Load(GetRandomTetromino(), typeof(GameObject)), new Vector2(5.0f, 20.0f), Quaternion.identity);  // спавнит тот блок, который выберет рандом 
+        
+    }
+
+    public bool CheckIsInsideGrid (Vector2 pos) {
+
+        return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
+
+    }
+
+    public Vector2 Round(Vector2 pos) {
+
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+
+    }
+
+    string GetRandomTetromino()
+    {
+        int randomTetromino = Random.Range(0, 6); // выирает рандом блок
+
+        string randomTetrominoName = "Prefabs/Tetromino_T";
+
+        switch (randomTetromino)
+        {
+        
+            case 0:
+                randomTetrominoName = "Prefabs/Tetromino_T";
+                break;
+            case 1:
+                randomTetrominoName = "Prefabs/Tetromino_Long";
+                break;
+            case 2:
+                randomTetrominoName = "Prefabs/Tetromino_Square";
+                break;
+            case 3:
+                randomTetrominoName = "Prefabs/Tetromino_J";
+                break;
+            case 4:
+                randomTetrominoName = "Prefabs/Tetromino_L";
+                break;
+            case 5:
+                randomTetrominoName = "Prefabs/Tetromino_S";
+                break;
+            case 6:
+                randomTetrominoName = "Prefabs/Tetromino_Z";
+                break;
+
+
+        }
+
+        return randomTetrominoName;
+
+    }
+
+}
---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour
+{
+    float fall = 0;
+    public float fallSpeed = 1;
+
+    public bool allowRotation = true;
+    public bool limitRotation = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        CheckUserInput();
+
+    }
+
+    void CheckUserInput() {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+
+            transform.position += new Vector3(1, 0, 0);
+
+            if (CheckIsValidPosition()) {
+
+                FindObjectOfType<Game>().UpdateGrid(this); 
+
+            } else {
+
+                transform.position += new Vector3(-1, 0, 0);
+            }
+
+
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+
+            transform.position += new Vector3(-1, 0, 0);
+
+            if (CheckIsValidPosition())
+            {
+
+                FindObjectOfType<Game>().UpdateGrid(this);
+
+            } else {
+
+                transform.position += new Vector3(1, 0, 0);
+            }
+
+        } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+
+            if (allowRotation) {
+
+                if (limitRotation){
+
+                    if (transform.rotation.eulerAngles.z >= 90) {
+
+                        transform.Rotate(0, 0, -90);
+                 
+                    } else {
+
+                        transform.Rotate(0, 0, -90);
+                    }
+
+                } else {
+
+                        transform.Rotate(0, 0, -90);
+                }
+
+                    if (CheckIsValidPosition())  {
+
+                    FindObjectOfType<Game>().UpdateGrid(this);
+
+                } else {
+                        if (limitRotation)  {
+
+                        if (transform.rotation.eulerAngles.z >= 90) {
+
+                                transform.Rotate(0, 0, -90);
+
+                            } else { 
+
+                                transform.Rotate(0, 0, 90);
+                            }
+
+                    } else {
+
+
+                       transform.Rotate(0, 0, 90); 
+
+                        }
+           }
+                               
+        }
+      
+            } else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed) {
+
+            transform.position += new Vector3(0, -1, 0);
+
+            if (CheckIsValidPosition()) {
+
+                FindObjectOfType<Game>().UpdateGrid(this);
+
+            } else {
+
+                transform.position += new Vector3(0, 1, 0);
+
+                FindObjectOfType<Game>().DeleteRow();
+
+                enabled = false;
+
+                FindObjectOfType<Game>().SpawnNextTetromino();
+            }
+
+            fall = Time.time;
+
+        }
+    }
+
+    bool CheckIsValidPosition() { 
+
+        foreach (Transform mino in transform) {
+
+            Vector2 pos = FindObjectOfType<Game>().Round (mino.position);
+
+            if (FindObjectOfType<Game>().CheckIsInsideGrid (pos) == false) {
+
+                return false;
+            }
+
+            if (FindObjectOfType<Game>().GetTransformAtGridPosition(pos) !=null && FindObjectOfType<Game>().GetTransformAtGridPosition(pos).parent != transform)
+            {
+
+                return false;
+
+            }
+
+        }
+
+        return true;
+
+    }
+
+}
